26c6dcf8dad435572c0b4df2a1543defa24418f0
Shirley Bismuth <shirley.bsth@gmail.com>
2019-03-10 13:30:58 +0100

    Complément du PHP initial en faisant apparaître les variables

--- variables.php
+++ variables.php
@@ -1,5 +1,22 @@
+<?php
+
 $NameofMovie = Indiana Jones;
 $AlreadySeen = True;
 $ReleaseYear = 1989;
 $Rating = 8.2;
 
+{
+    echo "Movie = $NameofMovie <br>";
+}
+
+if  ($AlreadySeen)
+{
+    echo "J'ai déjà vu ce film <br> " ;
+}
+ else {
+        echo "Je n'ai pas encore vu ce film <br>" ;
+}
+{
+    echo "$NameofMovie est sorti en $ReleaseYar et a reçu une note de $Rating /10";
+}
+ ?>
